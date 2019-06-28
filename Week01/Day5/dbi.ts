e1f666e579ee553d22fae7cd825ac67ca71502af
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-06-28 11:40:39 +0200

    initial commit

---
+++ Week01/Day5/dbi.ts
@@ -0,0 +1,33 @@
+'use strict'
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let a: number = 12;
+console.log(a);
+let b: number = 15;
+console.log(a);
+let c: number = 100;
+console.log(a);
+let d: number = 30;
+console.log(a);
+let e: number = 67;
+
+// String
+let myName: string = 'Janet, Kerubo';
+console.log(myName);
+
+let myAge: string = '30';
+console.log(myAge);
+
+ let myHeightinmetres: string ='1.69';
+ console.log(myHeightinmetres);
+
+ let maritalstatus: string = 'Not married';
+ console.log(maritalstatus);
+ console.log(true);
+
+
