a5548a0164f190824ca336328943a9c1d0898650
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-06-30 00:26:27 +0200

    initial commit

--- Week01/Day5/variablemutation.ts
+++ Week01/Day5/variablemutation.ts
@@ -20,33 +20,36 @@ console.log(c);
 
 let d: number = 125;
 // divide d's value by 5
-d /=5
+d /= 5;
 console.log(d);
 
 let e: number = 8;
-// what's the cube of e's value?
+// what's the cube of e's value
+e= e*e*e;
 console.log(e);
 
 let f1: number = 123;
 let f2: number = 345;
-console.log(false);
+console.log(f1>f2);
 // tell if f1 is bigger than f2 (as a boolean)
 
 let g1: number = 350;
 let g2: number = 200;
-console.log(true);
+console.log(g2*2>g1);
 // tell if the double of g2 is bigger than g1 (pras a boolean)
 
 let h: number = 1357988018575474;
-console.log(true);
+
+console.log(h%11==0);
 // tell if 11 is a divisor of h (as a boolean)
 
 let i1: number = 10;
 let i2: number = 3;
-console.log(true);
+console.log( (i1 > i2*i2) && (i1 < i2*i2*i2) );
 // tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
 
 let j: number = 1521;
 // tell if j is divisible by 3 or 5 (as a boolean)
-console.log(false);
+
+console.log(j%3==0||j%5==0);
 
