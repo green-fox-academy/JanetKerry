c9894ba8699e4207445b21c5bf12f35c502d9b15
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-06-30 17:00:50 +0200

    Initial Commit

---
+++ Week01/Day5/Seconds in a day.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
+
+const secondsInADay: number = 24 * 60 * 60;
+const minutesCoeff: number = 60;
+const hoursCoeff: number = 60 * 60;
+
+let remainingSeconds: number = secondsInADay - (currentHours * hoursCoeff + currentMinutes * minutesCoeff + currentSeconds);
+
+console.log('The day is over in ' + remainingSeconds + ' seconds!!!!');
