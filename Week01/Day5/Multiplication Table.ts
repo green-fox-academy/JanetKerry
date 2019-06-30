b6a90691afc576d8d993304a0f1b53b9c5ecb85e
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-06-30 17:40:09 +0200

    Initial commit

---
+++ Week01/Day5/Multiplication Table.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+let number: number = 15;
+
+// Create a program that
+// prints the multiplication table with number
+//
+// Example:
+// The number 15 should print:
+//
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
+
+
+let number1: number = 15;
+let number2: number = 1
+
+while (number2 < 11) {
+    console.log(number2 + ' * ' + number + ' = ' + number * number2);
+    number2 ++;
+}
---
+++ Week01/Day5/Parametric-average.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+let input: number = 10;
+let summ: number = 5;
+
+for (let i: number = 1; i <= input; i++) {
+    summ += i;
+
+    let avgg: number = summ / input;
+    console.log('The sum is: ' + summ + ' and the average is: ' + avgg);
+}
