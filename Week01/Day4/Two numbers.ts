f40467eb3e8e354e61fe039c0e5bb94e54973fc2
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-05 12:07:11 +0200

    Styling up

--- Week01/Day4/Two numbers.ts
+++ Week01/Day4/Two numbers.ts
@@ -2,25 +2,20 @@
 
 // Create a program that prints a few operations on two numbers: 22 and 13
 
-// Print the result of 13 added to 22
-
-// Print the result of 13 substracted from 22
-
-// Print the result of 22 multiplied by 13
-
-// Print the result of 22 divided by 13 (as a decimal fraction)
-
 // Print the remainder of 22 divided by 13
 let a: number = 22;
 let b: number = 13;
 
-console.log(a+b);
+// Print the result of 13 added to 22
+console.log(a + b);
 
-console.log(a-b);
+// Print the result of 13 substracted from 22
+console.log(a - b);
 
 
-console.log(a/b);
+// Print the result of 22 multiplied by 13
+console.log(a / b);
 
-
-console.log(a%b);
+// Print the result of 22 divided by 13 (as a decimal fraction)
+console.log(a % b);
 
