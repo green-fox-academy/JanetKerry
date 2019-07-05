baecfc22cb287fb74116645ca136fc1a17b39d09
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-05 09:53:17 +0200

    commit

---
+++ Week02/Day-01/Function Factorial.ts
@@ -0,0 +1,17 @@
+// -  Create a function called `factorio`
+//    that returns it's input's factorial
+'use strict';
+
+let NumbersToMultiply = 5;
+
+// -  Create a function called `factorio`
+//    that returns it's input's factorial
+function factorio(inputNumber: number): number {
+    let Factor: number = 1;
+    for (let i: number = 1; i <= inputNumber; i++) {
+        Factor *= i;
+    }
+    return Factor;
+}
+
+console.log(factorio(NumbersToMultiply));
