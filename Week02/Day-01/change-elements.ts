1edd7b83395f1f0adaed4a6cee3f4e0158a67df1
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-05 23:16:31 +0200

    w2d1

--- Week02/Day-01/change-elements.ts
+++ Week02/Day-01/change-elements.ts
@@ -5,13 +5,14 @@
 // -  Change the 8 to 4 with the `.map` method 
 // -  Print the fourth element as a test
 
-
-
-let numList = [1, 2, 3, 8, 5, 6];
-
-// pass a function to map
-const map1 = numList.map(x => x * 2);
-
-console.log(numList);
-
-
+let numlist: number[] = [1, 2, 3, 8, 5, 6];
+// -  Change the 8 to 4 with the `.map` method 
+let numlistChanged = numlist.map(function (value) {
+    if (value == 8) {
+        return 4;
+    } else {
+        return value;
+    }
+});
+// -  Print the fourth element as a test
+console.log(numlistChanged);
