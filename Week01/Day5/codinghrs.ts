d0bed2778661b020d3145c47f4c108c9bb2af170
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-06 22:51:41 +0200

    cleaning

--- Week01/Day5/codinghrs.ts
+++ Week01/Day5/codinghrs.ts
@@ -2,15 +2,14 @@
 
 // An average Green Fox attendee codes 6 hours daily
 // The semester is 17 weeks long
-//
+// if the attendee only codes on workdays.
+
+
 // Print how many hours is spent with coding in a semester by an attendee,
 // if the attendee only codes on workdays.
-//
+console.log("Hours spent coding:" + 17 * 6 * 5);
+
 // Print the percentage of the coding hours in the semester if the average
 // work hours weekly is 52
-
-
-
-console.log ("Hours spent coding:" +17*6*5);
-console.log ("coding percentage:"+ (6.0*5.0/52.0*100.0) + "%");
+console.log("coding percentage:" + (6.0 * 5.0 / 52.0 * 100.0) + "%");
 
