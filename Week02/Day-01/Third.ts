1b42081dcfd5b5297dc71bb8b893f383a9433eae
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-07 14:53:06 +0200

    Cleaning

--- Week02/Day-01/Third.ts
+++ Week02/Day-01/Third.ts
@@ -2,8 +2,8 @@
 
 // -  Create a variable named `magicNumbers`
 //    with the following content: `[1, 3, 5, 7]`
-// -  Print the third element of `magicNumbers`
 
 let magicNumbers: Number[] = [1, 3, 5, 7];
+// -  Print the third element of `magicNumbers`
 console.log(magicNumbers[2]);
 
