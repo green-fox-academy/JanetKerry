f73ab49539c79582c3b776b91b921efb725a29d6
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-09 21:39:35 +0200

    refactoring

--- Week01/Day5/Print even.ts
+++ Week01/Day5/Print even.ts
@@ -2,8 +2,10 @@
 
 // Create a program that prints all the even numbers between 0 and 500
 
-for (let i: number = 1; i < 500; i ++) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
 }
+let i = 0;
+do {
+    i += 2; // same as i = i + 2;
+    console.log(i);
+}
+while (i < 500)
