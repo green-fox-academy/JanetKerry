43fc6dcb501b59ff0345a11bee451f57bcdb867b
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-09 12:16:13 +0200

    PrintEachLine

---
+++ Week03/Day-02/02PrintEachLine.ts
@@ -0,0 +1,14 @@
+'use strict';
+// Write a program that opens a file called "my-file.txt", then prints
+// each line from the file.
+// If the program is unable to read the file (for example it does not exist),
+// then it should print the following error message: "Unable to read file: my-file.txt"
+
+declare function require(path: string): any;
+
+let fs = require('fs');
+
+fs.open('my-file.txt', 'w', function (err, file) {
+    if (err) throw err;
+    console.log('Unable to read file: my-file.txt');
+});
