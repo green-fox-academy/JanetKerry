2d5ad3d38185cf9f715c7c858eae9022d3c13667
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-13 19:10:42 +0200

    refactoring

--- Week02/Day-03/10RainbowBoxFunction.ts
+++ Week02/Day-03/10RainbowBoxFunction.ts
@@ -6,21 +6,40 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // Create a square drawing function that takes 2 parameters:
-// The square size, and the fill color,
 // and draws a square of that size and color to the center of the canvas.
-// Create a loop that fills the canvas with a rainbow of colored squares.
-
-const canvasWidth = 600;
-const canvasHeigth = 400;
-
-function calcColor(min, max, val) {
-    let minHue = 240, maxHue = 0;
-    let curPercent = (val - min) / (max - min);
-    let color = "hsl(" + ((curPercent * (maxHue - minHue)) + minHue) + ", 100%, 50%)";
-    return color;
+function DrawSquare(squareSize: number, fillColor: string) {
+    let topX = canvas.width / 2 - squareSize / 2;
+    let topY = canvas.height / 2 - squareSize / 2;
+    // The square size, and the fill color,
+    ctx.fillStyle = fillColor;
+    ctx.fillRect(topX, topY, squareSize, squareSize);
 }
 
-for (let i = 400; i > 0; i -= 1) {
-    ctx.fillStyle = calcColor(240, 0, i);
-    ctx.fillRect(canvasWidth / 2 - i / 2, canvasHeigth / 2 - i / 2, i, i);
+function ToColorComponent(color: number) {
+    let value = Math.abs(color % 256).toString(16);
+
+    if (value.length == 1) {
+        value = '0' + value;
+    }
+
+    return value;
+}
+
+let step = 1;
+
+console.log(step);
+
+let red = 0;
+let redStep = 1;
+let green = 85;
+let greenStep = 2;
+let blue = 170;
+let blueStep = 3
+// Create a loop that fills the canvas with a rainbow of colored squares.
+for (let i = canvas.width; i > 0; i -= step) {
+    DrawSquare(i, "#" + ToColorComponent(red) + ToColorComponent(green) + ToColorComponent(blue));
+
+    red += redStep;
+    green += greenStep;
+    blue += blueStep;
 }
