13008a5bfcc6957afd97d93d923948c564d102d1
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-14 18:52:18 +0200

    refactoring

--- Week02/Day-01/Matrix.ts
+++ Week02/Day-01/Matrix.ts
@@ -13,3 +13,19 @@
 // * size should depend on a variable
 // ** Relax, a matrix is just like an array
 
+
+let dimension: number = 8;
+let matrix: any[] = [];
+for (let i: number = 1; i <= dimension; i++) {
+    matrix[i - 1] = [];
+    for (let j: number = 1; j <= dimension; j++) {
+        if (i + j - 1 == dimension) {
+            matrix[i - 1][j - 1] = 1;
+        } else {
+            matrix[i - 1][j - 1] = 0;
+        }
+    }
+    console.log(matrix[i - 1].join(" "));
+}
+// -  Print this two dimensional list to the console
+
