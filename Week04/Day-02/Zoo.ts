c0dd66dad04e5d7ec8f588f5f4fcdd07d368a496
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-17 00:23:08 +0200

    zoo

---
+++ Week04/Day-02/Zoo.ts
@@ -0,0 +1,17 @@
+"use strict";
+
+export class Animal {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  getName() {}
+  breed() {}
+  gender() {}
+  food() {}
+  skinColor() {}
+}
+export default { Animal };
