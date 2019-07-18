ce5569899d366ccbb86dd6a1a895f3cd1191ab29
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-18 20:25:20 +0200

    refactoring

--- Week03/Day-02/04WriteSingleLine.ts
+++ Week03/Day-02/04WriteSingleLine.ts
@@ -1,16 +1,16 @@
-'use strict';
-
+"use strict";
+export {};
 // Write a function that is able to manipulate a file
 // By writing your name into it as a single line
 // The file should be named "my-file.txt"
 // In case the program is unable to write the file,
 // It should print the following error message: "Unable to write file: my-file.txt"
 
-declare function require(path: string): any
-const fs = require('fs');
+declare function require(path: string): any;
+const fs = require("fs");
 
 try {
-    fs.writeFileSync('my-file.txt', 'Janet Kerubo');
+  fs.writeFileSync("my-file.txt", "Janet Kerubo");
 } catch (e) {
-    console.log('Unable to write file: my-file.txt');
+  console.log("Unable to write file: my-file.txt");
 }
