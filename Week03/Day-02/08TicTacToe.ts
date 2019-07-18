86c04f3369e5f720028897662f013c6640c75bc4
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-18 20:46:07 +0200

    refactoring

--- Week03/Day-02/08TicTacToe.ts
+++ Week03/Day-02/08TicTacToe.ts
@@ -1,13 +1,26 @@
+"use strict";
+
+export = {};
+declare function require(path: string): any;
+
+const fs = require("fs");
+
 // Write a function that takes a filename as a parameter
 // The file contains an ended Tic-Tac-Toe match
 // We have provided you some example files (draw.txt, win-x.txt, win-o.txt)
 // Return "X", "O" or "Draw" based on the input file
 
-console.log(ticTacResult('win-o.txt'))
-// Should print "O"
+let filecontent: string = fs.readFileSync("win-x.txt", "utf-8");
+let lines: string[] = filecontent.split("\n");
 
-console.log(ticTacResult('win-x.txt'))
-// Should print "X"
+let elements: string[][] = [];
 
-console.log(ticTacResult('draw.txt'))
-// Should print "Draw"
+for (let i: number = 0; i < lines.length; i++) {
+  elements.push(lines[i].split(""));
+}
+
+console.log(elements);
+
+function equals(char1: string, char2: string, char3: string) {
+  return char1 === char2 && char2 === char3;
+}
