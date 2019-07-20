7226a5e94fc53c552501a367fc3f6732194fffcb
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-21 00:24:41 +0200

    w1d5 refactoring

--- Week03/Day-02/10ReversedLInes.ts
+++ Week03/Day-02/10ReversedLInes.ts
@@ -1,27 +1,30 @@
-'use strict';
+"use strict";
 
 // Create a method that decrypts reversed-lines.txt
 
-declare function require(path: string): any
-const fs = require('fs');
+declare function require(path: string): any;
+const fs = require("fs");
 
 try {
-    let fileContent: string = fs.readFileSync('reversed-lines.txt', 'utf-8');
-    console.log(decrypt(fileContent));
+  let fileContent: string = fs.readFileSync("ReversedLines.txt", "utf-8");
+  console.log(decrypt(fileContent));
 } catch (e) {
-    console.log(e.message);
+  console.log(e.message);
 }
 function decrypt(content: string): string {
-    let decryptedString: string = '';
-    let contentArray: string[] = content.split('\n');
-    let tempString: string = '';
+  let decryptedString: string = "";
+  let contentArray: string[] = content.split("\n");
 
-    for (let i: number = 0; i < contentArray.length; i++) { // goes through the lines
-        tempString = '';
-        for (let j: number = 0; j < contentArray[i].length; j++) { // go through the letters of the given line
-            tempString += contentArray[i].charAt(contentArray[i].length - 1 - j);
-        }
-        decryptedString += tempString + '\n';
+  let tempString: string = "";
+
+  for (let i: number = 0; i < contentArray.length; i++) {
+    // goes through the lines
+    tempString = "";
+    for (let j: number = 0; j < contentArray[i].length; j++) {
+      // go through the letters of the given line
+      tempString += contentArray[i].charAt(contentArray[i].length - 1 - j);
     }
-    return decryptedString;
+    decryptedString += tempString + "\n";
+  }
+  return decryptedString;
 }
--- Week03/Day-02/13Lottery.ts
+++ Week03/Day-02/13Lottery.ts
@@ -1,38 +1,37 @@
-'use strict';
+"use strict";
 
 // Create a method that find the 5 most common lottery numbers in lottery.csv
 
-declare function require(path: string): any
-const fs = require('fs');
+declare function require(path: string): any;
+const fs = require("fs");
 
 try {
-    let lotteryContent: string = fs.readFileSync('lottery.csv', 'utf-8');
-    let lotteryArray = lotteryContent.split('\n');
-    buildLotteryNumbersArray(lotteryArray);
+  let lotteryContent: string = fs.readFileSync("lottery.csv", "utf-8");
+  let lotteryArray = lotteryContent.split("\n");
+  buildLotteryNumbersArray(lotteryArray);
 } catch (e) {
-    console.log(e.message);
+  console.log(e.message);
 }
 
 function buildLotteryNumbersArray(lotteryArray: string[]): string[] {
-    let lotteryNumbersArray: string[] = [];
-    let lotteryNumbersObject = {};
-    let line: string[] = [];
-    let counter: number = 1;
+  let lotteryNumbersArray: string[] = [];
+  let lotteryNumbersObject = {};
+  let line: string[] = [];
+  let counter: number = 1;
 
-    lotteryArray.forEach(function (value, index) {
-        line = value.split(';');
-        for (let i: number = line.length - 1; i >= line.length - 5; i--) {
-            lotteryNumbersArray.push(line[i].replace('\r', ''));
-            if (lotteryNumbersObject.hasOwnProperty(line[i].replace('\r', ''))) {
-                lotteryNumbersObject[line[i].replace('\r', '')]++;
-            } else {
-                lotteryNumbersObject[line[i].replace('\r', '')] = 1;
-            }
-        }
-        counter++;
-    });
+  lotteryArray.forEach(function(value, index) {
+    line = value.split(";");
+    for (let i: number = line.length - 1; i >= line.length - 5; i--) {
+      lotteryNumbersArray.push(line[i].replace("\r", ""));
+      if (lotteryNumbersObject.hasOwnProperty(line[i].replace("\r", ""))) {
+        lotteryNumbersObject[line[i].replace("\r", "")]++;
+      } else {
+        lotteryNumbersObject[line[i].replace("\r", "")] = 1;
+      }
+    }
+    counter++;
+  });
 
-
-    console.log(lotteryNumbersObject);
-    return line;
+  console.log(lotteryNumbersObject);
+  return line;
 }
