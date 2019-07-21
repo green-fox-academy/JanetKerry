d8b64a31738d41c1b500100fb38ee53fed04a3d9
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-21 23:22:27 +0200

    refactoring

--- Week01/Day-05/cuboid.ts
+++ Week01/Day-05/cuboid.ts
@@ -1,8 +1,7 @@
-'use strict';
+"use strict";
 
 // Write a program that stores 3 sides of a cuboid as variables (floats)
 // The program should write the surface area and volume of the cuboid like:
-//
 // Surface Area: 600
 // Volume: 1000
 
@@ -10,15 +9,11 @@ let lengthCube: number = 5.3;
 let widthCube: number = 10;
 let heightCube: number = 2;
 
-
 let cubeVolume: number = lengthCube * widthCube * heightCube;
 let cubeSurface: number =
-    lengthCube * widthCube * 2
-    + widthCube * heightCube * 2
-    + lengthCube * heightCube * 2;
+  lengthCube * widthCube * 2 +
+  widthCube * heightCube * 2 +
+  lengthCube * heightCube * 2;
 
-
-console.log("Volume of the cube: " + cubeVolume);
-console.log("Surface of the cube: " + cubeSurface);
-
-
+console.log("Volume: " + cubeVolume);
+console.log("Surface Area: " + cubeSurface);
