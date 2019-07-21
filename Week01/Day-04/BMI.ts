be53fc5d9276e5227c83c7e5cd9c0bb6ae31a0a7
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-21 23:08:53 +0200

    My BMI

--- Week01/Day-04/BMI.ts
+++ Week01/Day-04/BMI.ts
@@ -1,7 +1,6 @@
-'use strict';
+"use strict";
 // Print the Body mass index (BMI) based on these values
 let massInKg: number = 81.2;
 let heightInM: number = 1.78;
 
 console.info(massInKg / (heightInM * heightInM));
-
