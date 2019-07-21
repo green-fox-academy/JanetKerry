d116f1f01082c63180f2e4cf04a1b2dc1119dd16
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-07-21 23:27:31 +0200

    refactoring

---
+++ Week01/Day-05/DefineBasicInfo.ts
@@ -0,0 +1,21 @@
+"use strict";
+
+// Define several things as a variable, then print their values
+
+// Your name as a string
+let myName: string = "Janet, Kerubo";
+console.log(myName);
+
+// Your age as a number
+let myAge: string = "30";
+console.log(myAge);
+
+// Your height in meters as a number
+let myHeightinmetres: number = 1.69;
+console.log(myHeightinmetres);
+
+// Whether you are married or not as a boolean
+// String
+let maritalstatus: string = "Not married";
+console.log(maritalstatus);
+console.log(true);
