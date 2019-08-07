a9f2826c5de8183c0f302786d680a1b1c12bafb5
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-08-07 19:28:38 +0200

    rendering todo list using template iteration

---
+++ Week06/Day-03/Todos/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+const todos = ['get up', 'survive', 'go back to bed'];
+
+// home page
+app.get('/', (req, res) => {
+  // render `home.ejs`
+  res.render('home', { todos: todos });
+});
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+//looping over an array
+todos.forEach(function(item, array) {
+  console.log(item);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html', { root: __dirname });
+});
