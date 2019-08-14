e248e26f765c18a337c1971b68e9d4680420ebdc
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-08-14 16:32:13 +0200

    Try doubling endpoint

--- Week07/Day-03/project/app.js
+++ Week07/Day-03/project/app.js
@@ -11,6 +11,33 @@ app.use(express.static('assets'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+//GET /doubling endpoint
+// app.get('/doubling', (req, res) => {
+//   if (isNaN(parseInt(req.query.input))) {
+//     res.json({ error: 'Please provide an input!' });
+//   } else {
+//     res.json({
+//       received: parseInt(req.query.input),
+//       result: parseInt(req.query.input) * 2
+//     });
+//   }
+// });
+
+app.get('/doubling', (req, res) => {
+  let input = req.query.input;
+  let result;
+  if (isNaN(input) == false) {
+    result = {
+      received: input,
+      result: input * 2
+    };
+  } else {
+    result = {
+      error: 'Please provide an input!'
+    };
+  }
+  res.send(result);
+});
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);
