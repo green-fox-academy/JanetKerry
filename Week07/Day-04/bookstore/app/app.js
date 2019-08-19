f151cc70a7f885ec56fae2777745927f38071665
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-08-19 23:25:19 +0200

    .

--- Week07/Day-04/bookstore/app/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const PORT = 3000;
-app.use(express.static('static'));
-
-const body = document.querySelector('body');
-
-const xhr = new XMLHttpRequest();
-xhr.onload = function() {};
