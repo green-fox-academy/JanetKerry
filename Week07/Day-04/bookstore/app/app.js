dfc70cff9dcf9f606b9eb546281658afada2d3ca
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-08-19 17:48:49 +0200

    bookstore endpoints

---
+++ Week07/Day-04/bookstore/app/app.js
@@ -0,0 +1,8 @@
+'use strict';
+const PORT = 3000;
+app.use(express.static('static'));
+
+const body = document.querySelector('body');
+
+const xhr = new XMLHttpRequest();
+xhr.onload = function() {};
--- Week07/Day-04/bookstore/server.js
+++ Week07/Day-04/bookstore/server.js
@@ -2,19 +2,36 @@
 
 const express = require('express');
 const mysql = require('mysql');
+const app = express();
 
 const connection = mysql.createConnection({
-  host: 'mysql',
+  host: 'localhost',
   user: 'root',
   password: 'Keruborocks247',
   database: 'bookstore'
 });
 
-const app = express();
+connection.connect(err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Database connected');
+  }
+});
 
-app.get('/book', (req, res) => {
+app.get('/titles', (req, res) => {
+  connection.query('SELECT book_name FROM book_mast; ', (err, rows) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(rows);
+    }
+  });
+});
+
+app.get('/fullList', (req, res) => {
   connection.query(
-    'SELECT book_name, author.aut_name, category.cate_descrip, pub_name, book_price ' +
+    'SELECT book_name, aut_name, cate_descrip, pub_name, book_price ' +
       'FROM book_mast inner join author on book_mast.aut_id = author.aut_id ' +
       'inner join category on book_mast.cate_id= category.cate_id ' +
       'inner join publisher on book_mast.pub_id= publisher.pub_id; ',
@@ -24,4 +41,20 @@ app.get('/book', (req, res) => {
   );
 });
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/app/titles.js', (req, res) => {
+  res.sendFile(__dirname + '/app/titles.js');
+});
+app.get('/static/titleslist.html', (req, res) => {
+  res.sendFile(__dirname + '/static/titleslist.html');
+});
+app.get('/static/fulllist.html', (req, res) => {
+  res.sendFile(__dirname + '/static/fulllist.html');
+});
+app.get('/app/fullList.js', (req, res) => {
+  res.sendFile(__dirname + '/app/fullList.js');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
