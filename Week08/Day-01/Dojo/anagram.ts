266ebc8d0de52134e394670ce52a66da8913ad96
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-08-21 20:02:16 +0200

    function & Test: anagram with Adam

--- Week08/Day-01/Dojo/anagram.ts
+++ Week08/Day-01/Dojo/anagram.ts
@@ -1,5 +1,24 @@
 'use strict';
 
-function isAnagram(parameterA: string, parameterB: string) {}
+function isAnagram(word1: string, word2: string): boolean {
+  word1 = word1
+    .split(' ')
+    .join('')
+    .toLowerCase();
+  word2 = word2
+    .split(' ')
+    .join('')
+    .toLowerCase();
+  if (word1 === word2) {
+    return true;
+  }
+  return false;
+}
 
 export { isAnagram };
+
+// isAnagram('A', 'A'); // expected output : true
+// isAnagram('A', 'B'); // expected output : false
+// TDD - Test Driven Development
+
+console.log(isAnagram('sIlEnT', 'listen'));
--- Week08/Day-01/Dojo/testAnagram.ts
+++ Week08/Day-01/Dojo/testAnagram.ts
@@ -3,12 +3,63 @@
 import test from 'tape';
 import { isAnagram } from './anagram';
 
+//test-1 - both letter are the same
 test(t => {
-  let stringOne: string = 'string1';
-  let stringTwo: string = 'string2';
+  let word1: string = 'A';
+  let word2: string = 'A';
 
   let outPut: boolean = true;
 
-  t.deepEqual(isAnagram(stringOne, stringTwo), outPut);
+  t.deepEqual(isAnagram(word1, word2), outPut);
+  t.end();
+});
+
+//test-2 Letters are different
+test(t => {
+  let word1: string = 'A';
+  let word2: string = 'B';
+
+  let outPut: boolean = false;
+
+  t.deepEqual(isAnagram(word1, word2), outPut);
+  t.end();
+});
+
+//test-3 space has no influence
+test(t => {
+  let word1: string = 'B ';
+  let word2: string = 'B';
+
+  let outPut: boolean = true;
+
+  t.deepEqual(isAnagram(word1, word2), outPut);
+  t.end();
+});
+
+//test-4 capital letter has no influence
+test(t => {
+  let word1: string = 'b';
+  let word2: string = 'B';
+
+  let outPut: boolean = true;
+
+  t.deepEqual(isAnagram(word1, word2), outPut);
+  t.end();
+});
+//test 5: checking length
+test(t => {
+  let word1: string = 'aaa';
+  let word2: string = 'bbb';
+
+  t.equal(word1.length, word2.length);
+  t.end();
+});
+//test 6: letter order has no influence
+test(t => {
+  let word1: string = 'aAbBccc';
+  let word2: string = 'cccBbAa';
+  let outPut: boolean = true;
+
+  t.deepEqual(isAnagram(word1, word2), outPut);
   t.end();
 });
