fcf7e89a44267819a5c3bba65af61475ba31caf5
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-09-02 17:23:25 +0200

    task(week-09/day-01): add promise-asynchronous

---
+++ Week09/Day-01/promise/async.js
@@ -0,0 +1,15 @@
+'use strict';
+// First, create a promise using the Promise constructor.
+
+// In the promise’s executor, immediately fulfill the promise with a value of
+// 'PROMISE VALUE'.
+
+// After the creation of the promise, pass console.log as the success handler to
+// the promise.
+
+// Finally, print out “MAIN PROGRAM” with console.log.
+
+const promise = new Promise(function(resolve, reject) {
+  resolve('PROMISE VALUE');
+}).then(console.log);
+console.log('MAIN PROGRAM');
