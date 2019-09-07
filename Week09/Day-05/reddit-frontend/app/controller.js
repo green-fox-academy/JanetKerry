933aedc8389551a98fa3348ec2435b750d9a2272
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-09-07 23:09:56 +0200

    reddit(week-09/day-04):controller.js

--- Week09/Day-05/reddit-frontend/app/controller.js
+++ Week09/Day-05/reddit-frontend/app/controller.js
@@ -1,10 +1,12 @@
 'use strict';
 
-//const model = require('./model');
+const model = require('./model');
 
 function index(req, res) {
-  console.log('rendering index');
-  res.render('index', {});
+  model.getTopics(results => {
+    console.log('rendering index');
+    res.render('index', { results });
+  });
 }
 
 module.exports = {
