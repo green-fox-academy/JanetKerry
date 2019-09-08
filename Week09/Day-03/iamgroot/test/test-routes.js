407e6b9dbe98f7e9e080ccce6765118c9ba8fa59
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-09-08 21:47:00 +0200

    task(week-09/day-03):change status code-error

--- Week09/Day-03/iamgroot/test/test-routes.js
+++ Week09/Day-03/iamgroot/test/test-routes.js
@@ -3,18 +3,32 @@
 const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
-​
-test('/groot endpoint1', (t) => {
-    request (app)
-  .get('/groot?somemessage=Grootonthedecks')
-  .set('accept', 'application/json')
-  end((err, resp)=>{
-    if (err) throw err;
-    t.equal(resp.status, 200);
-    t.same(resp.body, {
-        "received": "Grootonthedecks",
-        "translated": "I am Groot!"
+
+test('groot endpoint happypath', t => {
+  request(app)
+    .get('/groot?somemessage=Grootonthedecks')
+    .set('accept', 'application/json')
+    .end((err, resp) => {
+      if (err) throw err;
+      t.equal(resp.status, 200);
+      t.same(resp.body, {
+        received: 'Grootonthedecks',
+        translated: 'I am Groot!'
+      });
+      t.end();
     });
-    t.end();
 });
+
+test('groot endpoint error case', t => {
+  request(app)
+    .get('/groot')
+    .set('accept', 'application/json')
+    .end((err, resp) => {
+      if (err) throw err;
+      t.equal(resp.status, 500);
+      t.same(resp.body, {
+        error: 'somemessage undefined'
+      });
+      t.end();
+    });
 });
