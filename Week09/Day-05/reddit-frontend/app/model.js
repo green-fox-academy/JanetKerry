48c0c1e9cef54484c6aa1b45c2a2a86d03111afc
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-09-08 23:57:46 +0200

    reddit(week-09/day-04):add topic details

--- Week09/Day-05/reddit-frontend/app/model.js
+++ Week09/Day-05/reddit-frontend/app/model.js
@@ -9,7 +9,12 @@ const getTopics = callBack => {
   xhr.onload = () => {
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
-      callBack(response);
+      let result = [];
+
+      for (let i = 0; i < response.posts.length; ++i) {
+        result.push(parseTopic(response.posts[i]));
+      }
+      callBack(result);
     } else {
       callBack(null);
     }
@@ -17,6 +22,22 @@ const getTopics = callBack => {
 
   xhr.open('GET', API_BASE_URL);
   xhr.send();
+};
+
+const parseTopic = topic => {
+  let owner = topic.owner || 'anonymous';
+  const secondsPassed = Date.now() - topic.timestamp;
+
+  return {
+    id: topic.id,
+    title: topic.title,
+    url: topic.url,
+    timestamp: topic.timestamp,
+    score: topic.score,
+    user: owner,
+    vote: topic.vote,
+    secondsPassed: secondsPassed
+  };
 };
 
 module.exports = {
