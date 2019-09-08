31fe43101310cfe059a082a8dc377eb0cf108d4b
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-09-08 21:58:26 +0200

    task(week-09/day-03):req.query.somemessage

--- Week09/Day-03/iamgroot/routes.js
+++ Week09/Day-03/iamgroot/routes.js
@@ -3,12 +3,13 @@ const app = express();
 
 app.get('/groot', (req, res) => {
   if (req.query.somemessage === undefined) {
+    res.status(500);
     res.json({
-      error: 'I am Groot!'
+      error: 'somemessage undefined'
     });
   } else {
     res.json({
-      received: 'somemessage',
+      received: req.query.somemessage,
       translated: 'I am Groot!'
     });
   }
