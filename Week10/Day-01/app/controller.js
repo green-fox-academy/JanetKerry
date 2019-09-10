a331ac567d35aa5c26acd817e386642a31500b76
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-09-10 22:55:00 +0200

    feat(week10-day1): controller and model

--- Week10/Day-01/app/controller.js
+++ Week10/Day-01/app/controller.js
@@ -2,7 +2,7 @@
 
 // TODO: use model instead of mock
 //const model = require('./model');
-const model = require('./modelMock');
+const model = require('./modelMock'); //routing res and req from .js &connecting model to the views
 
 function index(req, res) {
   res.render('index', {});
@@ -11,8 +11,16 @@ function index(req, res) {
 function GET_playlist_tracks(req, res) {
   return model.getPlaylistTracks(req.query.playlist_id);
 }
+function index(req, res) {
+  res.render('index', {});
+}
+
+function GET_playlists(req, res) {
+  return model.getPlaylists();
+}
 
 module.exports = {
   index,
+  GET_playlists,
   GET_playlist_tracks
 };
---
+++ Week10/Day-01/app/model.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const getPlaylists = () => {
+  // TODO
+};
+
+const getPlaylistTracks = playlistId => {
+  // TODO
+};
+
+const postPlaylistTracks = (playlistId, track) => {
+  // TODO
+};
+
+const deletePlaylistTracks = (playlistId, trackId) => {
+  // TODO
+};
+
+module.exports = {
+  getPlaylists,
+  getPlaylistTracks,
+  postPlaylistTracks,
+  deletePlaylistTracks
+};
