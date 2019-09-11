5ae9525bb7e15589bf9d006f6e6e37397636dc6f
JanetKerry <50882107+JanetKerry@users.noreply.github.com>
2019-09-11 23:42:41 +0200

    feat(Week10)add views,images

--- Week10/Day-01/index.js
+++ Week10/Day-01/index.js
@@ -1,22 +1,76 @@
-'use strict';
-
 const express = require('express');
-const controller = require('./app/controller');
-
+const mysql = require('mysql');
 const PORT = 3000;
 const app = express();
+
+const connection = mysql.createConnection({
+  //creating connection
+  host: 'localhost',
+  user: 'root',
+  password: 'Keruborocks247',
+  database: 'foxplayer'
+});
 
 app.set('view engine', 'ejs');
 app.use(express.json()); //enable to parse req.body !
 app.use(express.static('static')); //return without rendering through application
 
-app.get('/', controller.index);
-
-app.get('/playlist-tracks', (req, res) => {
-  res.json(controller.GET_playlist_tracks(req, res));
+app.get('/', (req, res) => {
+  console.log('rendering index');
+  res.render('index');
 });
 
-app.get('/playlists', (req, res) => {
-  res.json(controller.GET_playlists(req, res));
+app.get('/playlist', (req, res) => {
+  // let playlistPromise = new Promise((resolve, reject) => {--why is it even here??????
+  connection.query(`SELECT * FROM playlists;`, (err, rows) => {
+    if (err) {
+      res.status(500);
+      console.log(err);
+    } else {
+      res.send(rows);
+    }
+  });
 });
+//});
+app.post('/playlist', (req, res) => {
+  connection.query(
+    `INSERT INTO playlists (playlist,system) VALUES ('${req.post.playlist}',0);`,
+    (err, rows) => {
+      if (err) {
+        res.status(500);
+        console.log(err);
+      } else {
+        res.status(200);
+      }
+    }
+  );
+});
+
+app.delete('/playlist', (req, res) => {
+  connection.query(
+    `DELETE FROM playlists WHERE id= ${req.query.id} and system <>1;`,
+    (err, rows) => {
+      if (err) {
+        res.status(500);
+        console.log(err);
+        res.send(JSON.stringify({ error: 'Database error occured!' }));
+      } else {
+        res.status(200);
+      }
+    }
+  );
+});
+
+app.get('/playlists_tracks', (req, res) => {
+  let query = 'SELECT * FROM tracks';
+  if (req.query && req.query.playlist_id) {
+    query = `${query} WHERE playlist_id=${req.query.playlist_id}`;
+  }
+  query += ';';
+
+  connection.query(query, (err, rows) => {
+    res.send(rows);
+  });
+});
+
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
